c8745fecca26fa44c3abf6161cf60953e680ca7f
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-11-11 15:53:23 -0300

    Testes não rodam

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
+const model = require('./models');
 
 const app = express();
 
 // não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
+});
+
+app.get('/products', async (_req, res) => {
+  const result = await model.productsModel.findAll();
+  res.status(200).json(result);
+});
+
+app.get('/products/:id', async (_req, res) => {
+  const result = await model.productsModel.findAll();
+  res.status(200).json(result);
 });
 
 // não remova essa exportação, é para o avaliador funcionar
