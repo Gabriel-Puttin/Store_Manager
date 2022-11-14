fc4a27bb9f0ce9f410b3fb98326ea01c157b7111
Gabriel Puttin <gabrielputtin@hotmail.com>
2022-11-14 10:09:37 -0300

    Finished 100% coverage in test

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -2,31 +2,23 @@ const productsService = require('../services/products.service');
 
 const HTTP_OK_STATUS = 200;
 const HTTP_NOT_FOUND_STATUS = 404;
-const HTTP_SERVER_ERROR_STATUS = 500;
+// const HTTP_SERVER_ERROR_STATUS = 500;
 
 const getAllProducts = async (_req, res) => {
-  try {
-    const result = await productsService.getAllProducts();
-    if (result.type !== null) {
-      return res.status(HTTP_NOT_FOUND_STATUS).send({ message: result.message });
-    }
-    res.status(HTTP_OK_STATUS).json(result.message);
-  } catch (error) {
-    res.status(HTTP_SERVER_ERROR_STATUS).send({ message: error.message });
+  const result = await productsService.getAllProducts();
+  if (result.type !== null) {
+    return res.status(HTTP_NOT_FOUND_STATUS).send({ message: result.message });
   }
+  res.status(HTTP_OK_STATUS).json(result.message);
 };
 
 const getProductsById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const result = await productsService.getProductsById(Number(id));
-    if (result.type !== null) {
-      return res.status(HTTP_NOT_FOUND_STATUS).send({ message: result.message });
-    }
-    res.status(HTTP_OK_STATUS).json(result.message);
-  } catch (error) {
-    res.status(HTTP_SERVER_ERROR_STATUS).send({ message: error.message });
+  const { id } = req.params;
+  const result = await productsService.getProductsById(Number(id));
+  if (result.type !== null) {
+    return res.status(HTTP_NOT_FOUND_STATUS).send({ message: result.message });
   }
+  res.status(HTTP_OK_STATUS).json(result.message);
 };
 
 module.exports = {
--- tests/unit/controllers/products.controller.test.js
+++ tests/unit/controllers/products.controller.test.js
@@ -9,6 +9,7 @@ const productsController = require('../../../src/controllers/products.controller
 const { allProducts } = require('../models/mocks/products.mocks');
 
 const HTTP_OK_STATUS = 200;
+const HTTP_NOT_FOUND_STATUS = 404;
 
 describe('Testes da camada Controllers', function () {
   describe('Teste o endpoint que lida com a requisição de todos os produtos', function () {
@@ -22,6 +23,51 @@ describe('Testes da camada Controllers', function () {
 
       expect(res.status).to.have.been.calledWith(HTTP_OK_STATUS);
       expect(res.json).to.have.been.calledWith(allProducts);
+    });
+
+    it('Testa a função "getAllProducts" em caso de erro', async function () {
+      const res = {};
+      const req = { body: {} };
+      res.status = sinon.stub().returns(res);
+      res.send = sinon.stub().returns();
+      sinon.stub(productsService, 'getAllProducts')
+        .resolves({ type: 'Invalid Product', message: 'Product not found' });
+      await productsController.getAllProducts(req, res);
+
+      expect(res.status).to.have.been.calledWith(HTTP_NOT_FOUND_STATUS);
+      expect(res.send).to.have.been.calledWith({ message: 'Product not found' });
+    });
+
+    afterEach(function () {
+      sinon.restore();
+    });
+  });
+
+  describe('Teste o endpoint que lida com a requisição de apenas um produto', function () {
+    it('Testa a função "getProductsById"', async function () {
+      const res = {};
+      const req = { params: { id: 1 } };
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      sinon.stub(productsService, 'getProductsById')
+        .resolves({ type: null, message: allProducts[0] });
+      await productsController.getProductsById(req, res);
+
+      expect(res.status).to.have.been.calledWith(HTTP_OK_STATUS);
+      expect(res.json).to.have.been.calledWith(allProducts[0]);
+    });
+
+    it('Testa a função "getProductsById" caso o produto não exista', async function () {
+      const res = {};
+      const req = { params: { id: 90 } };
+      res.status = sinon.stub().returns(res);
+      res.send = sinon.stub().returns();
+      sinon.stub(productsService, 'getProductsById')
+        .resolves({ type: 'Invalid Product', message: 'Product not found' });
+      await productsController.getProductsById(req, res);
+
+      expect(res.status).to.have.been.calledWith(HTTP_NOT_FOUND_STATUS);
+      expect(res.send).to.have.been.calledWith({ message: 'Product not found' });
     });
 
     afterEach(function () {
